84a08d2599fe621afa95180a2b6480ecc1479461
km20180269 <40146720180269@litec.ac.at>
2021-01-05 21:15:40 +0100

    improve slide effect

--- script/nav.js
+++ script/nav.js
@@ -19,7 +19,7 @@ window.addEventListener('load', () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.5}s`;
             }
         })
         burger.classList.toggle('toggle');
