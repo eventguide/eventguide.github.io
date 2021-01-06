85f6a109f715625fd6f9824a4a71ca968b61107e
km20180269 <40146720180269@litec.ac.at>
2021-01-06 14:53:28 +0100

    add to send mails

---
+++ script/contactform.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['submit'])) {
+
+        $firstName = $_POST['firstName'];
+        $surname = $_POST['surname'];
+        $mailFrom = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $mailTo = "office@eventguide.cc";
+        $headers = "From: " . $mailFrom;
+        $txt = "Du hast eine Email von " . $firstName . " " . $surname . " bekommen. \n\n" . $message;
+
+        mail($mailTo; $subject, $txt, $headers);
+
+        header("Location: index.php?mailsend");
+    }
+?>
